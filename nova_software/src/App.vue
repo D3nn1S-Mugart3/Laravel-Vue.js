bdfd15b9cf2a7e5c93e723bca62662bffd968cc0
d3nn1s-mugart3 <134119570+D3nn1S-Mugart3@users.noreply.github.com>
2024-09-08 10:44:07 -0600

    Create nova_software, proyecto Vue

---
+++ nova_software/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <UserList />
+  </div>
+</template>
+
+<script>
+import UserList from './components/UserList.vue';
+
+export default {
+  components: {
+    UserList
+  }
+};
+</script>
